6a0d01f07b409c8965a1efd5623d8e7c0d56a996
Kevin Uriel Manzano Rios <kmanzanor24@gmail.com>
2023-07-14 15:15:48 -0600

    Best time to buy and sell

---
+++ 4- 121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int res = 0;
+        if (prices.size() == 0)
+            return res;
+        int min = 100000;
+        for (auto i : prices)
+        {
+            if (i - min > res)
+                res = i - min;
+            if (i < min)
+                min = i;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << sol.maxProfit(prices);
+    return 0;
+}
